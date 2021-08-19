641b4987b842b1622c83263759695bee404aa33b
Ana Uribe <a01642346@tec.mx>
2021-08-19 20:27:04 +0000

    Tarea 1_A01642346

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,7 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    base = int(input("Dame la base: "))
+    altura = int(input("Dame la altura: "))
 
+    area = (base*altura)/2
 
-
+    print("El área es:", area)
 if __name__ == '__main__':
+
     main()
+    
